6aba3e61407bc684d0dc8ed9b1f306a139cad2b8
yraa99 <revo.reva125@gmail.com>
2026-09-20 00:27:07 +0700

    gui: expand security backup partition coverage

--- chatgpt/gui/special_task.py
+++ chatgpt/gui/special_task.py
@@ -30,6 +30,8 @@ class SpecialTaskTab(QWidget):
         "md_sec", "oeminfo", "factory", "devinfo", "oppostanvbk",
         "oppodycnvbk", "oppo_custom", "opporeserve", "asuskey",
         "board_info", "secure_storage", "secure", "certification",
+        "frp", "keymaster", "keystore", "tee", "trustzone", "tz",
+        "secdata", "storsec", "persistbak", "widevine", "drm",
     )
 
     def __init__(self, emmc, console):
